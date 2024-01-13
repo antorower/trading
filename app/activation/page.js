7be21914c29c5a821f2e6fe88df6074043616471
antorower <antonis.mastorakis@gmail.com>
2024-01-13 09:36:45 +0200

    Try2

--- app/activation/page.js
+++ app/activation/page.js
@@ -7,9 +7,7 @@ const Activation = async () => {
   return (
     <div className="flex flex-col gap-4 h-screen justify-center items-center">
       <Image src="/tick.svg" width={70} height={70} alt="tick" />
-      <div className="font-nova text-2xl text-white w-[450px] text-center flex justify-center">
-        {`We appreciate your presence ${user.username} and eagerly anticipate activating your account as soon as possible.`}
-      </div>
+      <div className="font-nova text-2xl text-white w-[450px] text-center flex justify-center">{`We appreciate your presence ${user.username} and eagerly anticipate activating your account as soon as possible.`}</div>
     </div>
   );
 };
