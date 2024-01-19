c3e486141e18cff56b4b6d49b1faae4dd260da86
antorower <antonis.mastorakis@gmail.com>
2024-01-19 20:08:06 +0200

    waiting for dns to update

--- app/api/get-settings/route.js
+++ app/api/get-settings/route.js
@@ -6,15 +6,8 @@ import { ErrorHandler } from "@/library/functions";
 
 export async function GET(req, context) {
   const user = await currentUser();
-  const response = new NextResponse();
-
-  // Set CORS headers
-  response.headers.set("Access-Control-Allow-Origin", "https://suckmypip.com"); // Make sure to replace with your actual domain
-  response.headers.set("Access-Control-Allow-Methods", "GET,OPTIONS");
-  response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
   if (!user) {
-    return response.json({ error: "Permissions required" }, { status: 400 });
+    return NextResponse.json({ error: "Permissions required" }, { status: 400 });
   }
 
   try {
@@ -23,14 +16,11 @@ export async function GET(req, context) {
     if (!doc) {
       const newSettings = new Settings();
       await newSettings.save();
-      doc = newSettings;
+      return NextResponse.json(newSettings);
     }
-    response.json(doc);
-    return response;
+    return NextResponse.json(doc);
   } catch (error) {
-    const errorHandlerResponse = await ErrorHandler(user, error, "Something went wrong, please try again", "/api/get-settings");
-    response.status(errorHandlerResponse.status ? errorHandlerResponse.status : 500);
-    response.json({ error: errorHandlerResponse.message ? errorHandlerResponse.message : "Something went wrong, please try again" });
-    return response;
+    const response = await ErrorHandler(user, error, "Something went wrong, please try again", "/api/get-settings");
+    return NextResponse.json({ error: response.message ? response.message : "Something went wrong, please try again" }, { status: response.status ? response.status : 500 });
   }
 }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -9,21 +7,6 @@ const nextConfig = {
         hostname: "img.clerk.com",
       },
     ],
-  },
-  async headers() {
-    return [
-      {
-        // Apply these headers for all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          // If you're not including credentials, you could use '*' but it's safer to specify the domain:
-          { key: "Access-Control-Allow-Origin", value: "https://www.suckmypip.com" },
-          { key: "Access-Control-Allow-Methods", value: "GET, OPTIONS, PATCH, DELETE, POST, PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ],
-      },
-    ];
   },
 };
 
