3c053ce027478b3dafd8c56934f3c6a8ffde01ff
antorower <antonis.mastorakis@gmail.com>
2024-01-19 16:17:42 +0200

    publish

--- app/dashboard/admin/layout.js
+++ app/dashboard/admin/layout.js
@@ -5,7 +5,7 @@ import UserBar from "@/components/UserBar/UserBar";
 export default function AdminLayout({ children }) {
   return (
     <div className="flex h-full">
-      <div className="h-full flex-1">{children}</div>
+      <div className="h-full flex-1 ">{children}</div>
       <div className="h-full">
         <UserBar />
       </div>
