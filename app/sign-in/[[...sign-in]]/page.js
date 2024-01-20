83bda849e77f09f523a05ea4449937db199386b1
Antonis <antonis.mastorakis@gmail.com>
2024-01-20 15:34:45 +0200

    Mentor fixed

--- app/sign-in/[[...sign-in]]/page.js
+++ app/sign-in/[[...sign-in]]/page.js
@@ -4,13 +4,16 @@ import { useSearchParams } from "next/navigation";
 
 export default function Page() {
   const params = useSearchParams();
+  const mentorId = params.get("id");
 
-  if (typeof localStorage !== "undefined") {
-    localStorage.setItem("mentor", params.get("id"));
+  if (typeof localStorage !== "undefined" && mentorId) {
+    localStorage.setItem("mentor", mentorId);
   }
+
   return (
     <div className="flex justify-center items-center h-full">
       <SignIn />
     </div>
   );
 }
+
