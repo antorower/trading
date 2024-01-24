209d858210e4138b86f348e1da89329c1c4e52c7
antorower <antonis.mastorakis@gmail.com>
2024-01-24 04:14:14 +0200

    No UserBar

--- app/dashboard/admin/layout.js
+++ app/dashboard/admin/layout.js
@@ -6,9 +6,7 @@ export default function AdminLayout({ children }) {
   return (
     <div className="flex h-full">
       <div className="h-full flex-1 ">{children}</div>
-      <div className="h-full">
-        <UserBar />
-      </div>
+      <div className="h-full">{/*<UserBar />*/}</div>
     </div>
   );
 }
