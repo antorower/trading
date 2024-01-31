901bbdf02f2878d80cc1e64e7dada50914219639
antorower <antonis.mastorakis@gmail.com>
2024-01-31 05:51:47 +0200

    tp sl fixed

--- app/api/open-trade/[account]/[balance]/route.js
+++ app/api/open-trade/[account]/[balance]/route.js
@@ -195,8 +195,8 @@ export async function GET(req, context) {
       const newLots = planPair.lastLots < initialLots ? (initialLots * (Math.random() * (1.08 - 1.03) + 1.03)).toFixed(2) : (initialLots * (Math.random() * (0.97 - 0.92) + 0.92)).toFixed(2);
       /*const takeProfit = await accountObj.GetTakeProfit(newLots);
       const stopLoss = await accountObj.GetStopLoss(newLots);*/
-      const takeProfit = Math.floor((await accountObj.GetTakeProfit(newLots)) / 100);
-      const stopLoss = Math.floor((await accountObj.GetStopLoss(newLots)) / 100);
+      const takeProfit = await accountObj.GetTakeProfit(newLots);
+      const stopLoss = await accountObj.GetStopLoss(newLots);
 
       const newTrade = {
         userId: user.id,
