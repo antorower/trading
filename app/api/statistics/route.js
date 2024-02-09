45009156c0ae139bc55a6a7f11e809dd461a89e0
antorower <antonis.mastorakis@gmail.com>
2024-02-09 05:07:59 +0200

    stats 11

--- app/api/statistics/route.js
+++ app/api/statistics/route.js
@@ -44,7 +44,7 @@ export async function GET(req, context) {
       const remainingProfitPercent = remainingProfit / capital;
       if (account.phase === 1 && remainingProfitPercent <= 0.044) oneTradeUpgradePhase1++;
       if (account.phase === 2 && remainingProfitPercent <= 0.05) oneTradeUpgradePhase2++;
-      if (account.phase === 2 && remainingProfitPercent <= 0.04) oneTradePayment++;
+      if (account.phase === 3 && remainingProfitPercent <= 0.04) oneTradePayment++;
       if (account.overallDrawdown > account.balance - account.dailyDrawdown * 0.8) oneTradeLose++;
 
       if (account.phase === 1) numberOfAccountsPhase1++;
