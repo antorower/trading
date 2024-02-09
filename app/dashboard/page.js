45f9fb57728dbefdc9b3fdbeca000db74e6727a3
antorower <antonis.mastorakis@gmail.com>
2024-02-09 06:42:57 +0200

    stats fixed 2

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -11,8 +11,9 @@ const Dashboard = () => {
 
   return (
     <div className=" text-white p-4 w-full h-full">
-      <div className="h-full flex justify-center items-center text-2xl">Welcome back, {user?.username}</div>
-      <Stats />
+      <div className="h-full flex justify-center items-center text-2xl">
+        <Stats />
+      </div>
     </div>
   );
 };
