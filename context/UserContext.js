96616b1c02550c8ff3989ec17c72eaa8de9e03f1
antorower <antonis.mastorakis@gmail.com>
2024-02-13 06:56:49 +0200

    wrong api

--- context/UserContext.js
+++ context/UserContext.js
@@ -54,7 +54,7 @@ export const UserContextProvider = ({ children }) => {
 
   const UpdateAccounts = async (selectedUser) => {
     try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/stats-end`);
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/stats-e0nd`);
 
       const data = await response.json();
       if (!response.ok) {
