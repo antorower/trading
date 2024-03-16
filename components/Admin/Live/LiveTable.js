9fc700fe272ecc940565693d951945656afbdfaa
antorower <antonis.mastorakis@gmail.com>
2024-03-16 03:43:25 +0200

    try stats fixed 1

--- components/Admin/Live/LiveTable.js
+++ components/Admin/Live/LiveTable.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import { useUserContext } from "@/context/UserContext";
 import TableWrapper from "../../TableWrapper";
 import LiveRow from "./LiveRow";
+import TableRow from "@/components/TableRow";
 
 const LiveTable = () => {
   const [liveAccountsPanelExpanded, setLiveAccountsPanelExpanded] = useState(true);
@@ -21,6 +22,10 @@ const LiveTable = () => {
 
   return (
     <TableWrapper title="Live Accounts" refresh={true} refreshFunction={UpdateAccounts} panelExpanded={liveAccountsPanelExpanded} setPanelExpanded={setLiveAccountsPanelExpanded}>
+      <TableRow>
+        <div>ena duo</div>
+        <div>tria tessera</div>
+      </TableRow>
       {liveAccounts && liveAccounts.length > 0 && liveAccounts.map((account) => <LiveRow key={account._id} account={account} />)}
     </TableWrapper>
   );
