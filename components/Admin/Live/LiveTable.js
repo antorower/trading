3334de0ef1ea09c505b30c936492e5eceb55322a
antorower <antonis.mastorakis@gmail.com>
2024-03-16 04:12:50 +0200

    live stats fixed

--- components/Admin/Live/LiveTable.js
+++ components/Admin/Live/LiveTable.js
@@ -29,11 +29,18 @@ const LiveTable = () => {
     setLiveAccounts(live);
 
     const phase1AccountsVar = live.filter((account) => account.phase === 1);
-    const phase1GoodVar = phase1AccountsVar.filter((account) => account.balance >= account.target * 0.978);
+    const phase1GoodVar = phase1AccountsVar.filter((account) => account.balance >= account.capital * 1.036);
     const phase1BadVar = phase1AccountsVar.filter((account) => account.balance <= account.capital * 0.94);
     setPhase1Accounts(phase1AccountsVar.length);
     setPhase1Good(phase1GoodVar.length);
     setPhase1Bad(phase1BadVar.length);
+
+    const phase2AccountsVar = live.filter((account) => account.phase === 2);
+    const phase2GoodVar = phase2AccountsVar.filter((account) => account.balance >= account.capital * 1.036);
+    const phase2BadVar = phase2AccountsVar.filter((account) => account.balance <= account.capital * 0.94);
+    setPhase1Accounts(phase2AccountsVar.length);
+    setPhase1Good(phase2GoodVar.length);
+    setPhase1Bad(phase2BadVar.length);
   }, [adminAccounts]);
 
   if (!liveAccounts || liveAccounts?.length === 0) {
@@ -47,10 +54,10 @@ const LiveTable = () => {
           Phase 1 Accounts: {phase1Accounts} | Good: {phase1Good} | Bad: {phase1Bad}
         </div>
         <div>
-          Phase 2 Accounts: {phase1Accounts} | Good: {phase1Good} | Bad: {phase1Bad}
+          Phase 2 Accounts: {phase2Accounts} | Good: {phase2Good} | Bad: {phase2Bad}
         </div>
         <div>
-          Phase 3 Accounts: {phase1Accounts} | Good: {phase1Good} | Bad: {phase1Bad}
+          Phase 3 Accounts: {phase3Accounts} | Good: {phase3Good} | Bad: {phase3Bad}
         </div>
       </TableRow>
       {liveAccounts && liveAccounts.length > 0 && liveAccounts.map((account) => <LiveRow key={account._id} account={account} />)}
