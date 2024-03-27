416663353aa6809657e215b4b38fd904edaf4f91
antorower <antonis.mastorakis@gmail.com>
2024-03-27 13:07:03 +0200

    most profitable trader try 2

--- app/api/get-most-profitable-trader/route.js
+++ app/api/get-most-profitable-trader/route.js
@@ -27,7 +27,7 @@ export async function GET(req, context) {
 
     // Check if there are payouts
     if (!payouts || !payouts.length) {
-      return NextResponse.json({ profits: 0 });
+      return NextResponse.json({ profits: [] });
     }
 
     // Sum amounts for each user and map usernames to the sum
@@ -41,15 +41,18 @@ export async function GET(req, context) {
     }, {});
 
     // Create an array with usernames and their total profits
-    const userProfits = Object.keys(profits).map((userId) => {
+    let userProfits = Object.keys(profits).map((userId) => {
       const user = users.find((u) => u.id === userId);
       return { username: user.username, totalProfit: profits[userId] };
     });
 
-    // Return the array of users and their profits
+    // Sort the array by totalProfit in descending order
+    userProfits.sort((a, b) => b.totalProfit - a.totalProfit);
+
+    // Return the sorted array of users and their profits
     return NextResponse.json(userProfits);
   } catch (error) {
-    const response = await ErrorHandler(user, error, "Something went wrong, please try again", "/api/get-user-profits");
+    const response = await ErrorHandler(null, error, "Something went wrong, please try again", "/api/get-user-profits");
     return NextResponse.json({ error: response.message ? response.message : "Something went wrong, please try again" }, { status: response.status ? response.status : 500 });
   }
 }
